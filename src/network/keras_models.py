6e6c79cbb86f3d76f0f0c28dfaf8b454f74c1a91
hallowf <alexandre.cortegaca@gmail.com>
2019-05-22 16:19:26 +0100

    Added Keras network for training

---
+++ src/network/keras_models.py
@@ -0,0 +1,93 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+from custom_exceptions import InvalidNetworkType
+
+class KModelBuilder(object):
+    """Builds network models based on input and returns them"""
+
+    def __init__(self, input_shape, network_type="unknown", classifications=3):
+        super(KModelBuilder, self).__init__()
+        self.classifications = classifications
+        self.network_type = network_type
+        self.input_shape = input_shape
+        self.models = {
+            "unknown": self.build_unknown_model,
+            "CIFAR10": self.build_CIFAR10,
+            "MLP": self.build_MLP_unknown,
+            "VGG": self.build_VGG
+        }
+        if network_type not in list(self.models.keys()):
+            raise InvalidNetworkType("Unknown network_type: %s" % network_type)
+
+    def return_model():
+        return self.models[self.network_type](self.input_shape)
+
+    def build_VGG(self, input_shape):
+        model = Sequential()
+        # input: input_shape images with 3 channels -> (x?, y?, z?) tensors.
+        # this applies 32 convolution filters of size 3x3 each.
+        model.add(Conv2D(32, (2, 2), activation='relu', input_shape=input_shape))
+        model.add(Conv2D(32, (2, 2), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (2, 2), activation='relu'))
+        model.add(Conv2D(64, (2, 2), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.classifications, activation='softmax'))
+        return model
+
+    def build_MLP_unknown(self, input_shape):
+        model = Sequential()
+        model.add(Conv2D(100, kernel_size=(2, 2), strides=(2, 2), activation='relu', input_shape=input_shape))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.classifications, activation='sigmoid'))
+        return model
+
+    # simple deep CNN
+    def build_CIFAR10(self, input_shape):
+        model = Sequential()
+        model.add(Conv2D(32, (2, 2), padding='same',
+                         input_shape=input_shape))
+        model.add(Activation('relu'))
+        model.add(Conv2D(32, (2, 2)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (2, 2), padding='same'))
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (2, 2)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(self.classifications))
+        model.add(Activation('softmax'))
+        return model
+
+    def build_unknown_model(self,input_shape):
+        # CNN model
+        model = Sequential()
+        model.add(Conv2D(100, kernel_size=(2, 2), strides=(2, 2), activation='relu', input_shape=input_shape))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(250, activation='relu'))
+        model.add(Dense(self.classifications, activation='softmax'))
+        return model
---
+++ src/network/keras_network.py
@@ -0,0 +1,130 @@
+import os, sys
+import keras
+from keras.models import load_model
+from keras.optimizers import Adam, SGD, Adamax, Adadelta, Adagrad, RMSprop, Nadam
+from sklearn.model_selection import train_test_split
+import numpy as np
+import cv2
+
+from network.keras_models import KModelBuilder
+from custom_exceptions import InvalidOptimizer, MissingDataSet, InvalidNetworkType
+
+
+class MainframeKeras(object):
+    """Loads the training_data,
+        builds a model based on network_type parameter
+        also accepts optimizer as parameter
+        fits training data into model and saves it"""
+
+    def __init__(self, identifier, network_type="unknown",optimizer="Adam"):
+        np.random.seed(3)
+        self.identifier = identifier
+        self.network_type = network_type
+        # BUG: i don't quit remember what this was needed for
+        self.classifications = 3
+        # create model name and trainind data name
+        self.training_data = None
+        self.training_data_name = "training/training_data_%s_balanced.npy" % identifier
+        self.model_name = "trained_models/%s_%s_%s.h5" % (self.network_type, self.optimizer, identifier)
+        # load the training_data
+        self.load_training_data = load_training_data()
+        # Map the optimizer and get rid of the others
+        self.optzr = self.map_optimizer(optimizer)
+
+    def load_training_data(self):
+        """Loads training data and raises MissingDataSet
+            if the file does not exist"""
+        if not os.path.isfile(self.training_data_name):
+            raise MissingDataSet("Failed to load %s" % self.training_data_name)
+        else:
+            self.training_data = np.load(self.training_data_name)
+
+    def map_optimizer(self, optimizer):
+        # QUESTION: is this even necessary the graphs shows all optimizers,
+        # is it due to the imports or the actual existence of them as instantiated objects in memory?
+        """Maps an optimizer and gets rid of the others."""
+        optimizers = {
+            "Adam": Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),
+            "SGD": SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False),
+            "RMSprop": RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0),
+            "Adagrad": Adagrad(lr=0.01, epsilon=None, decay=0.0),
+            "Adadelta": Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0),
+            "Adamax": Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0),
+            "Nadam": Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
+        }
+        if optimizer not in list(self.optimizers.keys()):
+            sys.stdout.write("Invalid optimizer: %s\n" % optimizer)
+            raise InvalidOptimizer("%s" % optimizer)
+        else:
+            optimizer = optimizers[optimizer]
+            del optimizers
+            return optimizer
+
+    def start(self):
+        ## input image dimensions
+        img_x, img_y = 160, 120
+        input_shape = (img_x, img_y, 1)
+        model = KModelBuilder(input_shape,self.network_type).return_model()
+
+        # train and test data
+        train = self.training_data[:-500]
+        test = self.training_data[-500:]
+
+        # Split data
+        X = np.array([i[0] for i in train]).reshape(-1, img_x, img_y, 1)
+        Y = [i[1] for i in train]
+        test_x = np.array([i[0] for i in test]).reshape(-1, img_x, img_y, 1)
+        test_y = [i[1] for i in test]
+
+        # compile the model
+        model.compile(loss="categorical_crossentropy", optimizer=self.optzr, metrics=['accuracy'])
+
+        # Create graph dir if necessary
+        graph_dir = "./Graph/%s/%s" % (self.network_type,self.optimizer)
+        if not os.path.isdir(graph_dir):
+            os.makedirs(graph_dir, exist_ok=True)
+
+        # tensorboard data callback
+        tbCallBack = keras.callbacks.TensorBoard(log_dir=graph_dir, histogram_freq=0, write_graph=True, write_images=True)
+
+        model.fit(X, Y, batch_size=20, epochs=30, validation_data=(test_x, test_y), callbacks=[tbCallBack])
+
+        if not os.path.isdir("trained_models"):
+            os.mkdir("trained_models")
+        # Build name: networkType_optimizer__trainingData.h5
+        # save weights post training
+        model.save(self.model_name)
+
+
+
+    # TODO:  maybe split data using train_test_split but this code below is broken
+    def old_start(self):
+        # split into test and train set
+        x_train, x_test, y_train, y_test = train_test_split(self.x, self.y, test_size=.2, random_state=5)
+
+
+
+        # convert class vectors to binary class matrices for use in categorical_crossentropy loss below
+        # number of action classifications, look for # Bug: in __init__
+        y_train = keras.utils.to_categorical(y_train, self.classifications)
+        y_test = keras.utils.to_categorical(y_test, self.classifications)
+
+        # Load model and optimizer based on user input
+        model = ModelBuilder(input_shape,self.network_type).return_model()
+
+        model.compile(loss="categorical_crossentropy", optimizer=self.optzr, metrics=['accuracy'])
+
+        graph_dir = "./Graph/%s/%s" % (self.network_type,self.optimizer)
+        if not os.path.isdir(graph_dir):
+            os.makedirs(graph_dir, exist_ok=True)
+        # tensorboard data callback
+        tbCallBack = keras.callbacks.TensorBoard(log_dir=graph_dir, histogram_freq=0, write_graph=True, write_images=True)
+
+        model.fit(x_train, y_train, batch_size=20, epochs=30, validation_data=(x_test, y_test), callbacks=[tbCallBack])
+        if not os.path.isdir("trained_models"):
+            os.mkdir("trained_models")
+        # Build name: networkType_optimizer_set(X)_trainingData.h5
+
+
+        # save weights post training
+        model.save(self.model_name)
